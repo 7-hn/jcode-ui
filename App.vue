2ed378f9c7c10aa6f6ae879808d33bdaea5c5f06
whn <746026935@qq.com>
2021-10-20 15:05:37 +0800

    初始化项目、封装button

---
+++ App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <j-button type="red" size="big" icon="shop">按钮</j-button>
+
+    <!--  显示  -->
+    <j-button animated="fade">
+      <div slot="hidden">$20000</div>
+      <div slot="visible"><i class="shop icon"></i>不要错过哈</div>
+    </j-button>
+  </div>
+</template>
+<script>
+import JButton from './packages/j-test-button'
+export default {
+  name: 'app',
+  components: {
+    JButton
+  },
+  data () {
+    return {}
+  }
+}
+</script>
