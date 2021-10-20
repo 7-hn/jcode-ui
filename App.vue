76182bec9956e0dfc37c23b49fa7d48e234451a8
whn <746026935@qq.com>
2021-10-20 15:42:37 +0800

    标题组件

--- App.vue
+++ App.vue
@@ -7,14 +7,24 @@
       <div slot="hidden">$20000</div>
       <div slot="visible"><i class="shop icon"></i>不要错过哈</div>
     </j-button>
+
+
+    <j-headline :level="1" icon="tag" size="mini">
+      标题
+      <div slot="sub">
+        子标题
+      </div>
+    </j-headline>
   </div>
 </template>
 <script>
 import JButton from './packages/j-test-button'
+import JHeadline from './packages/j-test-headline'
 export default {
   name: 'app',
   components: {
-    JButton
+    JButton,
+    JHeadline
   },
   data () {
     return {}
